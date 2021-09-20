12a87506f5fac1d5cf26322c8bf75a5edee1c292
Amitdag12 <gildag67@gmail.com>
2021-09-20 20:04:06 +0300

    .

--- GetPrice.php
+++ GetPrice.php
@@ -4,7 +4,7 @@ $Inputdata = json_decode(file_get_contents('php://input'), true);
 error_log($_GET["stockSymbol"]);
 error_log($_POST["stockSymbol"]);
 //aaaaaaaa
-$data = GetStockPage($Inputdata->{"stockSymbol"});
+$data = GetStockPage($Inputdata["stockSymbol"]);
 error_log(var_dump($Inputdata));
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode($data);
