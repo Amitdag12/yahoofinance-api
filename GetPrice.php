2ed490449ea5f40d005051dc86d2bf46e214d412
Amitdag12 <gildag67@gmail.com>
2021-09-22 20:36:37 +0300

    change page to qute

--- GetPrice.php
+++ GetPrice.php
@@ -2,7 +2,7 @@
 include 'index.php';
 $Inputdata = json_decode(file_get_contents('php://input'), true);
 //aaaaaaaa
-$data = GetStockPage($Inputdata["stockSymbol"]);
+$data = GetStockQuote($Inputdata["stockSymbol"]);
 //error_log(var_dump($Inputdata));
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode($data);
