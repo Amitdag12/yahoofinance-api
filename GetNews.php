52478113bfeaa4dddfb0fa30f0fbe5f4bd5b319a
Amitdag12 <66032598+Amitdag12@users.noreply.github.com>
2021-11-04 14:10:40 +0200

    .

--- GetNews.php
+++ GetNews.php
@@ -14,8 +14,8 @@
  $result=substr($result, strpos($result, '<ul class="My(0) P(0) Wow(bw) Ov(h)" data-reactid="3">'));
  $result=substr($result, 0, strpos($result, "</ul>"));
 $LiArray=SeperateStringToArray($result, "<li", "</li>");
-error_log(json_encode($LiArray[0]));
-error_log(json_encode($LiArray[1]));
+//error_log(json_encode($LiArray[0]));
+//error_log(json_encode($LiArray[1]));
 //GetLink($LiArray[0]);
 $response= [];
 for ($i=0; $i <count($LiArray) ; $i++) { 
@@ -58,7 +58,8 @@ function GetImage($li)
     $li=substr($li, strpos($li, "src=")+5);
   //  error_log($li);
     $li=substr($li, 0, strpos($li, '"'));
-  //  error_log($li);
+   error_log($li);
+   error_log(str_replace("\/","/",$li));
     return str_replace("\/","/",$li);
 }
 function GetHeadLine($li){
