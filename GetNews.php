54140ea490e33a07526f7e3b786b1a1fc4f8dc63
Amitdag12 <66032598+Amitdag12@users.noreply.github.com>
2021-11-04 13:48:19 +0200

    .

--- GetNews.php
+++ GetNews.php
@@ -14,7 +14,7 @@
  $result=substr($result, strpos($result, '<ul class="My(0) P(0) Wow(bw) Ov(h)" data-reactid="3">'));
  $result=substr($result, 0, strpos($result, "</ul>"));
 $LiArray=SeperateStringToArray($result, "<li", "</li>");
-error_log($LiArray);
+error_log(json_encode($LiArray));
 //GetLink($LiArray[0]);
 $response= [];
 for ($i=0; $i <count($LiArray) ; $i++) { 
