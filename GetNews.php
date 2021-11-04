232f863a1a6a2e544df2d5546f7924c0c811d674
Amitdag12 <66032598+Amitdag12@users.noreply.github.com>
2021-11-04 13:20:22 +0200

    .

--- GetNews.php
+++ GetNews.php
@@ -75,8 +75,8 @@ function GetParagraph($li){
     
     error_log("end a position:".strpos($li, "</a>"));
     $li=substr($li, strpos($li, "</a>")+2);
-    return 0;
     error_log($li);
+    return 0;
     error_log("end position:".strpos($li, "<p"));
     $li=substr($li, strpos($li, "<p")+1);
     error_log("a position:".strpos($li, ">"));
