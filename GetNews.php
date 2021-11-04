3f04dc65f0f619f539b5806b335ad1d9beae3dad
Amitdag12 <66032598+Amitdag12@users.noreply.github.com>
2021-11-04 15:43:13 +0200

    .

--- GetNews.php
+++ GetNews.php
@@ -68,6 +68,7 @@ function GetHeadLine($li){
   //  error_log($li);
   //  error_log("a position:".strpos($li, "<a"));
   $li=str_replace("<!-- /react-text -->","",$li);
+  $li=str_replace('"',"",$li);
     $li=substr($li, strpos($li, "<a")+2);
   //  error_log("end position:".strpos($li, ">"));
     $li=substr($li, strpos($li, ">")+1);
